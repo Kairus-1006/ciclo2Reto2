216cefe05c1bf49f0505306dd63f2698d5695ae1
Kairus-1006 <jeffer_ramos@hotmail.com>
2021-08-03 15:57:30 -0500

    se crea toda la estructura segun requerimientos del reto

---
+++ retoenclase/src/main/java/ciclo2/reto2/Herramienta.java
@@ -0,0 +1,24 @@
+package ciclo2.reto2;
+
+public class Herramienta {
+    
+    //Atributos
+    private Integer tiempoUso;
+    private boolean certificada;
+    private double valorBase;
+    
+    //Constantes
+    private final static double PRECIOBASE;
+    private final static Integer TIEMPOUSO;
+    private final static boolean CERTIFICADA;
+
+
+    //Constructores
+
+
+    //Metodos
+    public double calcularValor(){
+
+        return valor
+    }
+}
