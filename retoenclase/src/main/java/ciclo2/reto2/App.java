9f9aff437265df041a10f68d08b127074993c33f
Kairus-1006 <jeffer_ramos@hotmail.com>
2021-08-03 15:11:08 -0500

    Inicio, solo se crean las clases a usar

---
+++ retoenclase/src/main/java/ciclo2/reto2/App.java
@@ -0,0 +1,13 @@
+package ciclo2.reto2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
